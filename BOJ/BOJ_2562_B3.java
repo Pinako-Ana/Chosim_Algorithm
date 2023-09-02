c94b3930caf0527afbdc0ed38a2ac4f7c217a755
Pinako_Ana <fufru@naver.com>
2023-09-02 21:27:58 +0900

    first commit!

---
+++ BOJ/BOJ_2562_B3.java
@@ -0,0 +1,41 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.compare;
+
+public class BOJ_2562_B3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Node> myList = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            myList.add(new Node(num, i + 1));
+        }
+
+        myList.sort((o1, o2) -> compare(o2.number, o1.number));
+
+        System.out.println(myList.get(0).number);
+        System.out.println(myList.get(0).seq);
+
+    }
+
+    static class Node {
+        int number;
+        int seq;
+
+        public Node(int number, int seq) {
+            this.number = number;
+            this.seq = seq;
+        }
+    }
+
+
+}
